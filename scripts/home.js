22ecdcda7b721014bf818ad3caa43b349f160a68
Didarul Shahriar <didarul13579@gmail.com>
2025-08-30 13:10:28 +0600

    add money feature added

---
+++ scripts/home.js
@@ -0,0 +1,39 @@
+const validPin = 5678;
+
+document.getElementById('btn-add-money').addEventListener('click', function(e){
+    e.preventDefault();
+
+    const bank = document.getElementById('bank').value;
+    const accountNumber = document.getElementById('account-number').value;
+    const amount = parseInt(document.getElementById('add-amount').value);
+    const pin = parseInt(document.getElementById('add-pin').value);
+    const availabeBalance = parseInt(document.getElementById('available-balance').innerText);
+
+    if (bank === 'Select a bank'){
+        alert("Bank field can't be empty !");
+        return;
+    }
+    if (accountNumber.length < 11){
+        alert('Please provide a valid account number !');
+        return;
+    }
+    if (pin != validPin){
+        alert("Incorrect Pin !");
+        return;
+    }
+
+    const totalNewAvailableBalance = amount + availabeBalance;
+
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+})
+
+// toggling feature
+document.getElementById('add-button').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display = 'none';
+    document.getElementById('add-money-parent').style.display = 'block';
+})
+
+document.getElementById('cash-out-button').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display = 'none';
+    document.getElementById('cash-out-parent').style.display = 'block';
+})
